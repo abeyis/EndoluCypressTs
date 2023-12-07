a829d52376f84ea31e22406a01688ae73c4b1440
samet.ozcelik@abeyis.com <smtab142790@gmail.com>
2023-12-07 18:58:33 +0300

    Schema

--- cypress/e2e/storeApplicationAdd(Mağaza oluşturma).cy.ts
+++ cypress/e2e/storeApplicationAdd(Mağaza oluşturma).cy.ts
@@ -2,6 +2,8 @@ import LoginByEmailApi from "../api/login/LoginByEmailApi";
 import StoreApplicationAdd from "../api/login/StoreApplicationAdd";
 import User from "../models/User";
 import Store from "../models/Store";
+import SchemaUtils from "../utils/SchemaUtils";
+import Schemas from "../models/Schemas";
 
 describe('Store Application Add (Mağaza Oluşturma)', () => {
     
@@ -9,6 +11,8 @@ describe('Store Application Add (Mağaza Oluşturma)', () => {
     const user=new User();
     const store=new Store();
     const add=new StoreApplicationAdd()
+    const util=new SchemaUtils()
+    const schema=new Schemas()
     
     it('Login and Token', () => {
         
@@ -27,6 +31,8 @@ describe('Store Application Add (Mağaza Oluşturma)', () => {
         .then((response)=>{
             expect(response.status).to.be.equal(200)
             expect(response.body.message).to.equal('Mağaza başvurunuz alındı')
+            cy.log(response.body)
+            expect(util.getSchema(schema.getStoreAddResponseSchema(),response)).to.be.true
         })
 
     });
--- cypress/e2e/usercartsList(sepetim).cy.ts
+++ cypress/e2e/usercartsList(sepetim).cy.ts
@@ -1,19 +1,24 @@
 import LoginByEmailApi from "../api/login/LoginByEmailApi";
 import UsercartsList from "../api/login/UsercartsList";
+import Schemas from "../models/Schemas";
+
 import User from "../models/User";
+import SchemaUtils from "../utils/SchemaUtils";
 
 describe('login and usercarts (sepetim)', () => {
     
     const login=new LoginByEmailApi()
     const user =new User();
     const carts=new UsercartsList();
-
+    const schema=new Schemas()
+    const utils=new SchemaUtils()
     it('login and token', () => {
         login.createToken(user,user.getEmail())
         .then((response)=>{
             expect(response.status).to.be.equal(200)
             user.setToken(response.body.data.token)
             cy.log(response.body.data.token)
+            expect(utils.getSchema(schema.getLoginResponseSchema,response)).to.be.true
         })
     });
 
